8f884fe536f55ea67d828c02e569adb321b1b311
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-07-10 13:31:19 -0700

    [L10n] localize WinPhone81

---
+++ UsingResxLocalization/UsingResxLocalization.WinPhone81/Localize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+[assembly: Xamarin.Forms.Dependency(typeof(UsingResxLocalization.WinApp.Localize))]
+
+namespace UsingResxLocalization.WinApp
+{
+    public class Localize : UsingResxLocalization.ILocalize
+    {
+        public void SetLocale()
+        {
+            //
+        }
+
+        public System.Globalization.CultureInfo GetCurrentCultureInfo()
+        {
+            return System.Globalization.CultureInfo.CurrentUICulture;
+        }
+    }
+}
+
