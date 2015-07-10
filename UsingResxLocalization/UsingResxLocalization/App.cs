49338e55bca848dbaa542964f43b5025965b2857
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-07-10 12:40:35 -0700

    [L10n] localize Windows 8 app (might have broken iOS/Android)

---
+++ UsingResxLocalization/UsingResxLocalization/App.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Reflection;
+
+namespace UsingResxLocalization
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			System.Diagnostics.Debug.WriteLine("===============");
+			var assembly = typeof(App).GetTypeInfo().Assembly;
+			foreach (var res in assembly.GetManifestResourceNames()) 
+				System.Diagnostics.Debug.WriteLine("found resource: " + res);
+
+
+			if (Device.OS != TargetPlatform.WinPhone) {
+				DependencyService.Get<ILocalize> ().SetLocale ();
+				//Resx.AppResources.Culture = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+			}
+
+			var tabs = new TabbedPage ();
+
+			tabs.Children.Add (new FirstPage {Title = "C#", Icon="csharp.png"});
+
+			tabs.Children.Add (new FirstPageXaml {Title = "Xaml", Icon="xaml.png"});
+
+			MainPage = tabs;
+		}
+	}
+}
+
---
+++ UsingResxLocalization/UsingResxLocalization/PagesCs/FirstPage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using UsingResxLocalization.Resx;
+
+namespace UsingResxLocalization
+{
+	public class FirstPage : ContentPage
+	{
+		public FirstPage ()
+		{
+			// create UI controls
+			var myLabel = new Label ();
+			var myEntry = new Entry ();
+			var myButton = new Button ();
+			var myPicker = new Picker ();
+			myPicker.Items.Add ("0");
+			myPicker.Items.Add ("1");
+			myPicker.Items.Add ("2");
+			myPicker.Items.Add ("3");
+			myPicker.Items.Add ("4");
+
+
+			// apply translated resources
+			myLabel.Text = AppResources.NotesLabel;
+			myEntry.Placeholder = AppResources.NotesPlaceholder;
+			myPicker.Title = AppResources.PickerName;
+			myButton.Text = AppResources.AddButton;
+
+			var flag = new Image ();
+			flag.Source = ImageSource.FromFile ("flag.png"); // automatically resolved on iOS & Android
+
+			// button shows an alert, also translated
+			myButton.Clicked += async (sender, e) => {
+				var message = AppResources.AddMessageN;
+				if (myPicker.SelectedIndex <= 0) {
+					message = AppResources.AddMessage0;
+				} else if  (myPicker.SelectedIndex == 1) {
+					message = AppResources.AddMessage1;
+				} else {
+					message = String.Format(message, myPicker.Items[myPicker.SelectedIndex]);
+				}
+				await DisplayAlert (message, message, AppResources.CancelButton);
+			};
+
+			// add to screen
+			Content = new StackLayout {
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				Children = {
+					myLabel, 
+					myEntry, 
+					myPicker, 
+					myButton, 
+					flag},
+			};
+		}
+	}
+}
+
