b08d4d5699de38a533504dda1bc07ecb62ae0496
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-10-14 21:48:42 -0700

    preview

--- WorkingWithColors/WinUniversal/App.xaml.cs
+++ WorkingWithColors/WinUniversal/App.xaml.cs
@@ -15,9 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
-
-namespace WinUniversal
+namespace WorkingWithColors.WinUniversal
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
@@ -30,6 +28,9 @@ namespace WinUniversal
         /// </summary>
         public App()
         {
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
@@ -45,7 +46,7 @@ namespace WinUniversal
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-            //    this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
 
@@ -57,8 +58,6 @@ namespace WinUniversal
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
-                // Set the default language
-                rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
--- WorkingWithColors/WinUniversal/Properties/AssemblyInfo.cs
+++ WorkingWithColors/WinUniversal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinUniversal")]
+[assembly: AssemblyTitle("WorkingWithColors.WinUniversal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinUniversal")]
+[assembly: AssemblyProduct("WorkingWithColors.WinUniversal")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
